030b6e08c91512f5efd8812ee25786353e1ee6c0
Alexey Demyanov <deal110988@him-mail.ch>
2023-05-03 12:03:23 +0200

    added cheatsheet in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 document.title = `${new Date().toISOString()} TypeScript compiled`;
+
+const checkList = `
+npm i -D typescript to install TypeScript compiler
+npx tsc -v to check compiler version
+npx tsc --init to create tsconfig.json
+include and compilerOptions.rootDir parameters to reflect location of source files with TS code
+compilerOptions.outDir parameter to reflect where compiler will be placing final JS code files
+specify other compiler parameters where needed
+npx tsc to compile`;
