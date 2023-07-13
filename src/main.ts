ce7f501836401aa12c4191e0ac0c42410bb3e06a
Alexey Demyanov <deal110988@him-mail.ch>
2023-07-13 16:37:20 +0200

    Updated TS Cheatsheet

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,27 @@ include and compilerOptions.rootDir parameters to reflect location of source fil
 compilerOptions.outDir parameter to reflect where compiler will be placing final JS code files
 specify other compiler parameters where needed
 npx tsc to compile`;
+
+// Cheatsheet:
+
+type Role = {
+    title: string;
+    isAdmin: boolean;
+}
+
+type User = {
+    name: string;
+    roles: Role[]
+}
+
+type inferrredType1 = User['roles'];
+
+const propertyName = 'roles'; // has to be const to make it type literal
+type inferrredType2 = User[typeof propertyName];
+
+type inferrredType3 = User['roles'][number];
+
+const fruit = ['apple', 'egg', 'cucumber'] as const; // creating Union based on array
+type Fruit = typeof fruit[number];
+
+const someFruit: Fruit = 'egg';
